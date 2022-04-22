9adf41e9e8382c12f9b90eaa1f218ae9a80439cc
KIM YEJI <rladpwl0512@gmail.com>
2022-04-22 17:19:39 +0900

    refactor: 상태값으로 데이터 관리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import { useState } from 'react';
 
 function Header(props) {
-  return <header className="App-header">
+  return <header className="App-header" onClick={(event) => {
+    event.preventDefault();
+    props.onChangeMode();
+  }}>
     <h1><a href="/">{props.title}</a></h1>
   </header>
 }
 
 function Nav(props) {
-  const lists = props.topics.map((prop) => <li key={prop.id}><a href={'/read/' + prop.id}>{prop.title}</a></li>)
+  const lists = props.topics.map((prop) => <li key={prop.id}><a href={'/read/' + prop.id} onClick={(event) => {
+    event.preventDefault();
+    props.onChangeMode(prop.id);
+  }}>{prop.title}</a></li>)
   return <nav>
     <ol>
       {lists}
@@ -16,7 +23,6 @@ function Nav(props) {
 }
 
 function Article(props) {
-  console.log(props);
   return <article>
     <h2>{props.title}</h2>
     {props.body}
@@ -24,16 +30,42 @@ function Article(props) {
 }
 
 function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
   const topics = [
     {id: 1, title: 'html', body: 'html is ... '},
     {id: 2, title: 'css', body: 'css is ... '},
     {id: 3, title: 'javascript', body: 'javascript is ... '},
   ]
+
+  let content = null;
+  let title = null;
+  let body = null; 
+
+  if(mode === 'WELCOME') {
+    title = 'Welcome';
+    body = 'Hello, web';
+  } else if(mode === 'READ') {
+    for(let topic of topics) {
+      if(topic.id === id) {
+        title = topic.title;
+        body = topic.body;
+      }
+    }
+  }
+  content = <Article title={title} body={body}></Article>
+
+
   return (
     <div className="App">
-      <Header title="REACT"></Header>
-      <Nav topics={topics}></Nav>
-      <Article title="Welcome" body="Hello, Web"></Article>
+      <Header title="WEB" onChangeMode={() => {
+        setMode('WELCOME');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(_id) => {
+        setMode('READ');
+        setId(_id);
+      }}></Nav>
+      {content}
     </div>
   );
 }
